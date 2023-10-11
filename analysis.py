9bdb7feb69e14d20da37574b0ac4672b62b1e9fe
Leigh Tanji <leigh.tanji@tufts.edu>
2023-10-10 21:19:48 -0400

    Pipeline Start

--- analysis.py
+++ analysis.py
@@ -2,6 +2,15 @@ import numpy as np
 import sklearn.linear_model
 import sklearn.pipeline
 import sklearn.model_selection
+import sklearn.feature_selection
+from sklearn.feature_extraction.text import CountVectorizer
+
+#Create a pipeline:
+def make_pipeline():
+    bow_pipeline = sklearn.pipeline.Pipeline([
+        ("step1", sklearn.feature_extraction.text())
+    ])
+
 
 #Use at least 3 folds, searching over at least 5 possible hyperparameter configurations to avoid overfitting.
 def cross_validation(x_NF, y_N, n_folds):
@@ -16,4 +25,15 @@ def cross_validation(x_NF, y_N, n_folds):
 #get folds
 #get probabilities associated with each fold
 
+
+############### Cleaning USING SKLEAN FUNCTIONS #####################
+def clean_words(web_list, reviews_list, stars_list, n_folds):
+    bow_classifier = sklearn.pipeline.Pipeline([
+        ('bow feature extractor', CountVectorizer(ngram_range=(1,1), min_df=1, max_df=1.0, binary=False)),
+        ('classifying', sklearn.linear_model.LogisticRegression())
+        ('cross validating', sklearn.model_selection.RepeatedKFold(n_splits = n_folds, n_repeats = 1, random_state = 1))
+        ])
+    
+    x_N = vectorizer.fit_transform(reviews_list)
+    vectorizer.transform()
 
